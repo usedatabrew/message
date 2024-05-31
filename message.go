193cc29d14848562c8850e37e3fbdf4ab81f2bad
Vladyslav Len <heavenly.beta@gmail.com>
2024-05-31 23:21:50 +0200

    feat(): extending message object with sourceType

--- message.go
+++ message.go
@@ -5,17 +5,19 @@ import "sync"
 // Message represents a Blink message and contains raw data object
 // linked to the data stream and event.
 type Message struct {
-	lock   sync.RWMutex
-	Event  Event  `json:"event" yaml:"event"`
-	Stream string `json:"stream" yaml:"stream"`
-	Data   Data   `json:"data" yaml:"data"`
+	lock       sync.RWMutex
+	Event      Event  `json:"event" yaml:"event"`
+	SourceType string `json:"sourceType" yaml:"sourceType"`
+	Stream     string `json:"stream" yaml:"stream"`
+	Data       Data   `json:"data" yaml:"data"`
 }
 
-func NewMessage(event Event, stream string, data []byte) *Message {
+func NewMessage(event Event, sourceType, stream string, data []byte) *Message {
 	return &Message{
-		Event:  event,
-		Stream: stream,
-		Data:   NewData(data),
+		Event:      event,
+		Stream:     stream,
+		SourceType: sourceType,
+		Data:       NewData(data),
 	}
 }
 
@@ -25,6 +27,10 @@ func (m *Message) SetStream(stream string) {
 
 func (m *Message) GetStream() string {
 	return m.Stream
+}
+
+func (m *Message) GetSourceType() string {
+	return m.SourceType
 }
 
 func (m *Message) SetEvent(event Event) {
--- message_test.go
+++ message_test.go
@@ -2,8 +2,9 @@ package message
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func getTierMessage() []byte {
@@ -11,34 +12,34 @@ func getTierMessage() []byte {
 }
 
 func TestData_AccessProperty(t *testing.T) {
-	msg := NewMessage(Snapshot, "flights", getTierMessage())
+	msg := NewMessage(Snapshot, "postgres", "flights", getTierMessage())
 	assert.Equal(t, float64(123), msg.Data.AccessProperty("mes.id"))
 	assert.Equal(t, "1231", msg.Data.AccessProperty("message"))
 }
 
 func TestData_SetProperty(t *testing.T) {
-	msg := NewMessage(Snapshot, "flights", getTierMessage())
+	msg := NewMessage(Snapshot, "postgres", "flights", getTierMessage())
 	msg.Data.SetProperty("ai_result_1111", "example_result_data")
 	assert.Equal(t, "example_result_data", msg.Data.AccessProperty("ai_result_1111"))
 }
 
 func TestData_DopProperty(t *testing.T) {
-	msg := NewMessage(Snapshot, "flights", getTierMessage())
+	msg := NewMessage(Snapshot, "postgres", "flights", getTierMessage())
 	msg.Data.DropProperty("ai_result_1111")
 	assert.Equal(t, nil, msg.Data.AccessProperty("ai_result_1111"))
 }
 
 func TestData_MarshalJSON(t *testing.T) {
-	msg := NewMessage(Snapshot, "flights", getTierMessage())
+	msg := NewMessage(Snapshot, "postgres", "flights", getTierMessage())
 	props := msg.Data.AccessProperties([]string{"message", "array"})
 	assert.Equal(t, map[string]any{
-		"array": []interface{}{float64(1), float64(12), float64(33)},
+		"array":   []interface{}{float64(1), float64(12), float64(33)},
 		"message": "1231",
 	}, props)
 }
 
 func TestData_Where(t *testing.T) {
-	msg := NewMessage(Snapshot, "flights", getTierMessage())
+	msg := NewMessage(Snapshot, "postgres", "flights", getTierMessage())
 	data := msg.Data.Where("mes.id", "=", 123)
 	fmt.Println(data)
 }
